b72c7cbdd0cf3529fdf6a485dbf774ca7d29d457
M3L-N1K <93345052+M3L-N1K@users.noreply.github.com>
2021-10-29 16:17:59 +0300

    T-01
    IfStatementDemo

---
+++ src/com/company/Main.java
@@ -0,0 +1,13 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// Обозначаем переменные и присваиваем занчения
+    int a, b = 5, c, d = 3;
+
+    System.out.println("b равно 5, d равно 3");
+       if (b > d) System.out.println("b больше d");
+       if (b < d) System.out.println("Ошибка");
+    }
+}
